b3c0ea4273eb1fe83716fab9cb1547f4f2686c6e
Thiago Garcia <thiagogds14@gmail.com>
2011-02-13 19:54:20 -0200

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblioteca::Application.config.secret_token = 'e3cb6213726ff9d974318ef916fc712abed892b823c79907d1ad85dfea7361be4fa72b66f4fb901ab336f264e5e0110d97f1e0187aed65a44d1ef4100ff431a6'
